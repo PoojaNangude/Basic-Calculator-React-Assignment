43fe8d7a86dcf0245f5d4f1fd043eb917616e2ba
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-11-16 15:48:37 +0530

    DMAS of BODMAS

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,47 +1,59 @@
 import { render } from '@testing-library/react';
 import React, {useState,useEffect} from 'react';
 // In advanced calculator
-
-const  eval = (num1,opr,num2) =>{
-    const [exp,setExp]=useState(0);
-    switch(opr){
-        case '+':
-            setExp(parseFloat(num1)+parseFloat(num2));
-            break;
-        case '-':
-            setExp(parseFloat(num1)-parseFloat(num2));
-            break;
-        case '/':
-            setExp(parseFloat(num1)/parseFloat(num2));
-            break;
-        case '*':
-            setExp(parseFloat(num1)*parseFloat(num2));
-            break;
-    }
-    return(exp.toString());
-}
-
+//Does not handle brackets
 const Calculator = () => {
-
 const [expression,setExpression]=useState('');
 const [ans,setAns]=useState(0);
+let exp=0;
+
+const evaluate =(num1,opr,num2)=>{
+    switch(opr){
+        case '+':
+            exp=parseFloat(num1)+parseFloat(num2);
+            break;
+
+        case '-':
+            exp=parseFloat(num1)-parseFloat(num2);
+            break;
+
+        case '/':
+            exp=parseFloat(num1)/parseFloat(num2);
+            break;
+
+        case '*':
+            exp=parseFloat(num1)*parseFloat(num2);
+            break;
+    }
+    return(parseFloat(exp));
+}
+
 
 const handleSubmit = (e) =>{
     e.preventDefault();
-    let pref=['/','*','+','-'];
+    let pref=['/','*'];
     let separators = ['\\\+','\\\-','\\\/','\\\*','\\\(','\\\)'];
     let splitexp=expression.split(new RegExp("([" + separators.join("") + "])+"));
 
     for(let opr of pref){
         for(let i=0;i<splitexp.length;i++){
             if(splitexp[i]===opr){
-                let num1=splitexp[i-1];
-                let num2=splitexp[i+1];
-                const ans=eval(num1,opr,num2);
-                console.log(ans);
+                console.log(splitexp[i-1],opr,splitexp[i+1]);
+                exp=evaluate(splitexp[i-1],opr,splitexp[i+1]);
+                console.log(exp);
+                splitexp[i-1]=exp;
+                splitexp.splice(i,2);
+                console.log(splitexp);
             }
         }
     }
+
+    while(splitexp.length!==1){
+        splitexp[0]=evaluate(splitexp[0],splitexp[1],splitexp[2]);
+        splitexp.splice(1,2);
+    }
+    console.log(splitexp[0]);
+    setAns(splitexp[0]);
 }
 
     return (
