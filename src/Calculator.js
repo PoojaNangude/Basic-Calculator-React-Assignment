dc6d04409e213cde8576e3d64c9168115deb296a
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-11-16 20:03:40 +0530

    precedence pending

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,82 +3,156 @@ import React, {useState,useEffect} from 'react';
 // In advanced bracket calculator
 //Trying to handle brackets
 const Calculator = () => {
-const [expression,setExpression]=useState('');
-const [ans,setAns]=useState(0);
-let exp=0;
 
-const evaluate =(num1,opr,num2)=>{
-    switch(opr){
-        case '+':
-            exp=parseFloat(num1)+parseFloat(num2);
-            break;
+    const [expression,setExpression]=useState('');
+    const [ans,setAns]=useState(0);
+    let exp=0;
 
-        case '-':
-            exp=parseFloat(num1)-parseFloat(num2);
-            break;
+    const evaluate =(num1,opr,num2)=>{
+        switch(opr){
+            case '+':
+                exp=parseFloat(num1)+parseFloat(num2);
+                break;
 
-        case '/':
-            exp=parseFloat(num1)/parseFloat(num2);
-            break;
+            case '-':
+                exp=parseFloat(num1)-parseFloat(num2);
+                break;
 
-        case '*':
-            exp=parseFloat(num1)*parseFloat(num2);
-            break;
+            case '/':
+                exp=parseFloat(num1)/parseFloat(num2);
+                break;
+
+            case '*':
+                exp=parseFloat(num1)*parseFloat(num2);
+                break;
+
+            case '(':
+                break;
+        }
+        return(parseFloat(exp));
     }
-    return(parseFloat(exp));
-}
 
+    const precendence = (x) =>{
+        if(x==='('||x===')')
+        return(0);
+        else
+        if(x==='+'||x==='-')
+        return(1);
+        else
+        if(x==='*'||x==='/')
+        return(2);
+        else
+        return(3);
+    }
 
-const handleSubmit = (e) =>{
-    e.preventDefault();
-    let pref=['/','*'];
-    let separators = ['\\\+','\\\-','\\\/','\\\*','\\\(','\\\)'];
-    let splitexp=expression.split(new RegExp("([" + separators.join("") + "])+"));
+    const infixToPostfix = (exp) =>{
+        let stack=[];
+        let opertors=[];
+        let output='';
 
-    for(let opr of pref){
-        for(let i=0;i<splitexp.length;i++){
-            if(splitexp[i]===opr){
-                console.log(splitexp[i-1],opr,splitexp[i+1]);
-                exp=evaluate(splitexp[i-1],opr,splitexp[i+1]);
-                console.log(exp);
-                splitexp[i-1]=exp;
-                splitexp.splice(i,2);
-                console.log(splitexp);
+        for(let chr of exp){
+            if(!isNaN(chr)){output=output+chr;}
+
+            else
+            if(chr==='('){stack.push(chr);}
+
+            else
+            if(chr===')'){
+                while(stack[stack.length-1] !== '('){
+                    output=output+stack.pop();
+                }
+                stack.pop();
+            }
+
+            // else
+            // if(precendence(chr)<=precendence(stack[stack.length-1])){
+            //     while(precendence(chr)<=precendence(stack[stack.length-1])){
+            //         output=output+stack.pop();
+            //     }
+            //     stack.push(chr);
+            // }
+
+            else{stack.push(chr);} 
+        }
+
+        if((stack.length-1)!==-1){
+            while((stack.length-1)!==-1){
+                output=output+stack.pop();
             }
         }
+
+
+
+        // for(let chr of exp){
+
+        //     if(!isNaN(chr)){output=output+chr;}
+
+        //     else
+        //     if(chr==='('){stack.push(chr);}
+
+        //     else
+        //     if(chr===')'){
+        //         while(stack[stack.length-1]!=='('){
+        //             output=output+stack.pop();
+        //         }
+        //         stack.pop();
+        //     }
+
+        //     else
+        //     if(precendence(chr)<=precendence(stack[stack.length-1]) && (stack.length-1)!==-1){
+        //         while(precendence(chr)<=precendence(stack[stack.length-1]) && (stack[stack.length-1])!==-1){
+        //             output=output+stack.pop();
+        //         }
+        //         stack.push(chr);
+        //     }
+
+        //     else{stack.push(chr);}
+        // }
+
+        //     if((stack.length-1)!==-1){
+        //         while((stack.length-1)!==-1){
+        //             output=output+stack.pop();
+        //         }
+        //     }
+        //return(output);
+
+        console.log(output);
+        console.log(stack,stack.length);
+
+        }
+    
+
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        let postfix=infixToPostfix(expression);
+        console.log(postfix);
     }
 
-    while(splitexp.length!==1){
-        splitexp[0]=evaluate(splitexp[0],splitexp[1],splitexp[2]);
-        splitexp.splice(1,2);
-    }
-    console.log(splitexp[0]);
-    setAns(splitexp[0]);
-}
+        return (
+            <div>
+                <h1>Advanced Calculator</h1>
+                <form>
 
-    return (
-        <div>
-            <h1>Advanced Calculator</h1>
-            <form>
+                    <div>
+                        <label htmlFor="expression">Enter mathematical expression : </label>
+                        <input
+                        type="text"
+                        id="expression"
+                        name="expression"
+                        value={expression}
+                        onChange={(e) => setExpression(e.target.value)}
+                        />
+                    </div>
+                    <br></br>
 
-                <div>
-                    <label htmlFor="expression">Enter mathematical expression : </label>
-                    <input
-                    type="text"
-                    id="expression"
-                    name="expression"
-                    value={expression}
-                    onChange={(e) => setExpression(e.target.value)}
-                    />
-                </div>
-                <br></br>
+                    <button type="submit" onClick={handleSubmit}>Submit</button>
 
-                <button type="submit" onClick={handleSubmit}>Submit</button>
+                    <h3>ANS: {ans}</h3>
 
-                <h3>ANS: {ans}</h3>
-            </form>
-        </div>
-    );
+                </form>
+            </div>
+        );
 }
 
 export default Calculator;
