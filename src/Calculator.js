147050c16f7088c92b16377f2295622924202be8
Pooja Nangude <poojanangude@Poojas-MacBook-Air.local>
2020-11-16 11:17:32 +0530

    in progress

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,84 +1,63 @@
 import { render } from '@testing-library/react';
 import React, {useState,useEffect} from 'react';
 // In advanced calculator
+
+const  eval = (num1,opr,num2) =>{
+    const [exp,setExp]=useState(0);
+    switch(opr){
+        case '+':
+            setExp(parseFloat(num1)+parseFloat(num2));
+            break;
+        case '-':
+            setExp(parseFloat(num1)-parseFloat(num2));
+            break;
+        case '/':
+            setExp(parseFloat(num1)/parseFloat(num2));
+            break;
+        case '*':
+            setExp(parseFloat(num1)*parseFloat(num2));
+            break;
+    }
+    return(exp.toString());
+}
+
 const Calculator = () => {
 
-const [firstNum,setFirstNum]=useState(0);
-const [secondNum,setSecondNum]=useState(0);
-const [choice,setChoice]=useState('');
+const [expression,setExpression]=useState('');
 const [ans,setAns]=useState(0);
-const [prev,setPrev]=useState([]);
 
 const handleSubmit = (e) =>{
     e.preventDefault();
-    console.log('called before');
-    switch(choice){
-        case '+':
-            setAns(parseFloat(firstNum)+parseFloat(secondNum));
-            break;
+    let pref=['/','*','+','-'];
+    let separators = ['\\\+','\\\-','\\\/','\\\*','\\\(','\\\)'];
+    let splitexp=expression.split(new RegExp("([" + separators.join("") + "])+"));
 
-        case '-':
-            setAns(parseFloat(firstNum)-parseFloat(secondNum));
-            break;
-        
-        case '*':
-            setAns(parseFloat(firstNum)*parseFloat(secondNum));
-            break;
-
-        case '/':
-            setAns(parseFloat(firstNum)/parseFloat(secondNum));
-            break;
+    for(let opr of pref){
+        for(let i=0;i<splitexp.length;i++){
+            if(splitexp[i]===opr){
+                let num1=splitexp[i-1];
+                let num2=splitexp[i+1];
+                const ans=eval(num1,opr,num2);
+                console.log(ans);
+            }
+        }
     }
 }
 
-
-useEffect(()=>{
-    const record={id: new Date().getTime().toString(),firstNum,choice,secondNum,ans};
-    console.log(record);
-    setPrev((prev) => {
-        return [...prev,record];
-    });
-    console.log('in useeffect',ans);
-},[ans]);
-
-useEffect(()=>{
-    console.log('in useeffect',prev);
-},[prev]);
-
     return (
         <div>
-            <h1>Basic Calculator</h1>
+            <h1>Advanced Calculator</h1>
             <form>
 
                 <div>
-                    <label htmlFor="firstNum">Num 1 : </label>
-                    <input  
-                    type="number"
-                    id="firstNum"
-                    name="firstNum"
-                    value={firstNum}
-                    onChange={(e) => setFirstNum(e.target.value)}
-                    />
-                </div>
-                <br></br>
-
-                <div>
-                    <label htmlFor="secondNum">Num 2 : </label>
+                    <label htmlFor="expression">Enter mathematical expression : </label>
                     <input
-                    type="number"
-                    id="secondNum"
-                    name="secondNum"
-                    value={secondNum}
-                    onChange={(e) => setSecondNum(e.target.value)}
+                    type="text"
+                    id="expression"
+                    name="expression"
+                    value={expression}
+                    onChange={(e) => setExpression(e.target.value)}
                     />
-                </div>
-                <br></br>
-
-                <div onChange={(e) =>setChoice(e.target.value)}>
-                    <input type="radio" value="+" name="choice" />Add
-                    <input type="radio" value="-" name="choice" />Subtract
-                    <input type="radio" value="*" name="choice" />Product
-                    <input type="radio" value="/" name="choice" />Divide
                 </div>
                 <br></br>
 
@@ -86,15 +65,6 @@ useEffect(()=>{
 
                 <h3>ANS: {ans}</h3>
             </form>
-            {
-                prev.map((record,index)=>{
-                    const {id,firstNum,choice,secondNum,ans}=record;
-                    return (
-                        <div key={id}>
-                            <h4>{firstNum} {choice} {secondNum} = {ans}</h4>
-                        </div>
-                    );
-                })}
         </div>
     );
 }
